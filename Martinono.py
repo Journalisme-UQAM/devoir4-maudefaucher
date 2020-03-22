fd93bb68d0997acdf6c605631195ddb11264a00a
maudefaucher <59701318+maudefaucher@users.noreply.github.com>
2020-03-22 15:51:04 -0400

    Add files via upload

--- Martinono.py
+++ Martinono.py
@@ -13,7 +13,7 @@ motsrecherches = csv.reader(fichier)
 next(motsrecherches)
 
 a = []
-bigrams = []
+bigram = []
 
 for mots in motsrecherches:
     # print(mots[3])
@@ -22,17 +22,18 @@ for mots in motsrecherches:
     # print(tokens)
 
     fr = SnowballStemmer("french")
-    racines = [fr.stem(mot) for mot in word_tokenize(mots[3])if mot not in stopwords.words('french') and mot not in string.punctuation and mot != "’" and mot != "..." and mot != "«" and mot != "»" and mot != "``"]
+
+    racines = [fr.stem(mot) for mot in word_tokenize(mots[3])if mot not in stopwords.words("french") and mot not in string.punctuation and mot != "’" and mot != "..." and mot != "«" and mot != "»" and mot != "``"]
     # print(racines)
 
     for racine in racines:
         a.append(racine)
 
     for x, y in enumerate(mots[:-1]): 
-	    bigrams="{} {} {}".format(racines[x], racines[x+1])
+	    bigrams="{} {}".format(racines[x], racines[x+1])
         if "islam" in bigrams or "musulm" in bigrams:
         print(bigrams)
         bigram.append(bigrams)
 
 freq = Counter(bigram)
-print(freq.most_common(50))
+print(freq.most_common(50)) 
